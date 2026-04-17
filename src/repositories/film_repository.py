4c5d5d04c62311e5ec1943c601a4d3022bacf394
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2026-04-17 20:44:28 -0300

    Add film retrieval by ID and improve error handling
    Implements the ability to fetch specific film records by their unique identifier.
    
    Enhances the service layer with robust error handling to ensure consistent HTTP responses for missing resources, invalid IDs, and internal server errors.
    
    Refactors ID processing logic into the repository layer to improve the separation of concerns and overall code structure.

--- src/repositories/film_repository.py
+++ src/repositories/film_repository.py
@@ -1,8 +1,11 @@
-from bson import ObjectId
 from src.db.film_db import film_collection
+from bson import ObjectId
 
 def create_repository(film_dict):
     return film_collection.insert_one(film_dict)
+
+def get_by_id_repository(_id):
+    return film_collection.find_one({"_id": ObjectId(_id)})
     
 def list_repository():
     return film_collection.find()
--- src/services/film_service.py
+++ src/services/film_service.py
@@ -3,9 +3,6 @@ from bson import ObjectId
 from src.schemas.film_schema import *
 from src.repositories.film_repository import *
 
-def filter_object(_id):
-    return ObjectId(_id)
-
 def format_id(film):
     film["_id"] = str(film["_id"])
     return film
@@ -14,24 +11,50 @@ def film_dict(film):
     return film.model_dump(mode="json")
 
 def create_service(film: Film):
-    data = film_dict(film)
-    result = create_repository(data)
+    try:
+        data = film_dict(film)
+        result = create_repository(data)
+        
+        return {
+            "message": "Film Created",
+            "id": str(result.inserted_id)
+        }
+        
+    except HTTPException:
+        raise
     
-    return {
-        "message": "Film Created",
-        "id": str(result.inserted_id)
-    }
+    except Exception as e:
+        print("error: ", e)
+        raise HTTPException(
+            status_code= 500,
+            detail= "Internal server error"
+        )
+
+def get_by_id_service(_id):
+    try:
+        product = get_by_id_repository(_id)
+        if product:
+            format_id(product)
+            return product
+        else:
+            raise HTTPException(status_code=404 ,detail="Product not found")
+    except:
+       raise HTTPException(status_code=404, detail="Invalid ID")
     
 def list_service():
-    films = [format_id(film) for film in list_repository()]
+    try:
+        films = [format_id(film) for film in list_repository()]
+        
+        length = len(films)
+        
+        return {
+            "films": films, 
+            "length": length
+        }
     
-    length = len(films)
-    
-    return {
-        "films": films, 
-        "length": length
-    }
-    
+    except HTTPException:
+        raise
+        
 def update_service(_id, film):
     data = film_dict(film)
     result = update_repository(_id, data)
